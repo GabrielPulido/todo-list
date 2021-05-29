13a71e9e493f1fe5de68f7150a7a0847a3201bbd
Gabriel Pulido <GabrielPulido0056@gmail.com>
2021-05-29 10:48:59 -0700

    added live server

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    open: 'Google Chrome'
+  },
   output: {
-    filename: 'main.js', 
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
