5fe061eb9aa12fbe4fe1b4cfea2b1a71f47aa1ef
Gabriel Pulido <GabrielPulido0056@gmail.com>
2021-05-31 09:12:25 -0700

    Created Unique ID Pattern in HTML

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import Subtask from "./subtask";
 import { objectSummary } from "./utilities";
 import { isTask } from "./task";
 
-//Create Default Project
 let proj = Project();
-
 proj.setTitle('Default Project');
-console.log(`Project Title: ${proj.getTitle()}`);
 
-//create tasks
-let task1 = Task(`Default Task 1`, `Description`, `5/15/21`, `Low`, false);
+let task1 = Task(`Default Task 1`, `This is the Description`, `5/15/21`, `Low`, false);
 let task2 = Task(`Start Backend`, `connect to database & find hosting`, `Finished`, `High`, true);
 let task3 = Task(`Finish Frontend`, `dom manipulation w/ react/js`, `tomorrow`, `Medium`, false);
 let task4 = Task(`Unit Testing`, `test some things`, `Done`, `Low`, true);
@@ -23,5 +19,3 @@ let subtask2 = Subtask('Delete Line 32', false);
 
 task1.addSubtasks([subtask1, subtask2]);
 
-console.log(task1.getSubtasks());
-console.log(objectSummary(task1.getSubtasks()[1]));
